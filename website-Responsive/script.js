93af41abccb06a1d7fa193798a168d7cfeeaa298
vandanaqburst <vandana@qburst.com>
2014-09-03 13:53:50 +0530

    Files to make responsive

---
+++ website-Responsive/script.js
@@ -0,0 +1,111 @@
+/* 
+	author: istockphp.com
+*/
+jQuery(function($) {
+	
+	$("a.topopup").click(function() {
+			loading(); // loading
+			setTimeout(function(){ // then show popup, deley in .5 second
+				loadPopup(); // function show popup 
+			}, 500); // .5 second
+	return false;
+	});
+	
+	/* event for close the popup */
+	$("div.close").hover(
+					function() {
+						$('span.ecs_tooltip').show();
+					},
+					function () {
+    					$('span.ecs_tooltip').hide();
+  					}
+				);
+	
+	$("div.close").click(function() {
+		disablePopup();  // function close pop up
+	});
+	
+	$(this).keyup(function(event) {
+		if (event.which == 27) { // 27 is 'Ecs' in the keyboard
+			disablePopup();  // function close pop up
+		}  	
+	});
+	
+	$("div#backgroundPopup").click(function() {
+		disablePopup();  // function close pop up
+	});
+	
+	$('a.livebox').click(function() {
+		alert('Hello World!');
+	return false;
+	});
+	
+
+	 /************** start: functions. **************/
+	function loading() {
+		$("div.loader").show();  
+	}
+	function closeloading() {
+		$("div.loader").fadeOut('normal');  
+	}
+	
+	var popupStatus = 0; // set value
+	
+	function loadPopup() { 
+		if(popupStatus == 0) { // if value is 0, show popup
+			closeloading(); // fadeout loading
+			$("#toPopup").fadeIn(0500); // fadein popup div
+			$("#backgroundPopup").css("opacity", "0.7"); // css opacity, supports IE7, IE8
+			$("#backgroundPopup").fadeIn(0001); 
+			popupStatus = 1; // and set value to 1
+		}	
+	}
+		
+	function disablePopup() {
+		if(popupStatus == 1) { // if value is 1, close popup
+			$("#toPopup").fadeOut("normal");  
+			$("#backgroundPopup").fadeOut("normal");  
+			popupStatus = 0;  // and set value to 0
+		}
+	}
+	/************** end: functions. *************/
+}); // jQuery End
+
+//For json data input
+
+var cards = [];
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
+    $.getJSON('cards.json', function(data){
+        cards = data;
+       
+    });
+});
+function comboOnChange(cardCombo) {
+    
+    
+    var selCardType = cardCombo.options[cardCombo.selectedIndex].getAttribute("value");
+    var i;
+    for(i=0; i < cards.length; i++){
+        if(cards[i].cardtype == selCardType){
+            document.getElementById("cardNum").value = cards[i].CardNumber;
+            document.getElementById("secur").value = cards[i].Security;
+            document.getElementById("month").value = cards[i].Expiresonmonth;
+            document.getElementById("dates").value = cards[i].Expiresondate;
+            document.getElementById("year").value = cards[i].Expiresonyear;
+        }
+        else if(selCardType == "select"){
+            document.getElementById("cardNum").value = "";
+            document.getElementById("secur").value = "";
+            document.getElementById("month").selectedIndex = 0;
+            document.getElementById("dates").selectedIndex = 0;
+            document.getElementById("year").selectedIndex = 0;
+        }
+    }
+}
+
+function checkform(){
+        alert("Login Successful");
+        setTimeout(function() {window.location = "https://mail.google.com/mail" });
+    }
